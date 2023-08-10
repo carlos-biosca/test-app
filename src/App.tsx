eb8c565b2ed48392ec495942ebb834bf4588f195
carlos-biosca <cbiosca82@gmail.com>
2023-08-10 19:28:49 +0200

    react + tailwind + ui library

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+
+
+function App() {
+
+  return (
+    <>
+        <div>
+        <Button>Click me</Button>
+        <Label htmlFor="email">Your email address</Label>
+      </div>
+    </>
+  )
+}
+
+export default App
